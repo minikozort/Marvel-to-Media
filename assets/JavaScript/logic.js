b6629dc50bab8b54b3e86cb30a9d7e25144ccf4d
brianapgil <brianapgil@gmail.com>
2024-05-29 21:00:28 -0400

    Debugged search history buttons

--- assets/JavaScript/logic.js
+++ assets/JavaScript/logic.js
@@ -4,7 +4,9 @@ const searchSubmit = document.querySelector('#search-button');
 let character = JSON.parse(localStorage.getItem('character'));
 
 function handleSearch(event) {
-    event.preventDefault();
+    if (event) {
+        event.preventDefault();
+    }
     if (!characterInput.value) {
         alert('You need a search input value!');
         return;
@@ -13,10 +15,15 @@ function handleSearch(event) {
         character = [];
     }
     let characterChoice = characterInput.value;
-    character.push(characterChoice);
-    localStorage.setItem('character', JSON.stringify(character));
+    if (event) {
+        character.push(characterChoice);
+        localStorage.setItem('character', JSON.stringify(character));
+    }
+    
 
     characterInput.value = "";
+    getMarvelApi(characterChoice);
+    searchMoviesByName(characterChoice)
     searchHistory();
 }
 
@@ -24,16 +31,27 @@ function searchHistory() {
     $("#search-history-buttons").html("");
     for (const c of character) {
       const searchHistoryCharacter = $("<button>");
-      searchHistoryCharacter.addClass("history of search button")
-      const aTag = $("<a>");
-      searchHistoryCharacter.append(aTag);
-      aTag.text(c).addClass("button secondary");
+      searchHistoryCharacter.addClass("history-of-search-button button secondary").text(c);
       $("#search-history-buttons").append(searchHistoryCharacter);
-      searchHistoryCharacter.on('click', function (event) {
-        event.preventDefault();
-        $("#custom-search-input").val(c);
-      });
     }
   }
 
+
+$("#search-history-buttons").on('click', '.history-of-search-button', function (event) {
+    event.preventDefault();
+    const character = $(event.target);
+    console.log(character.text());
+    $("#custom-search-input").val(character.text());
+    handleSearch();
+
+  });
 searchSubmit.addEventListener('click', handleSearch);
+
+
+
+
+
+
+
+
+
