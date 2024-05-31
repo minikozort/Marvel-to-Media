16e5e178acb4a914b9d7ec03137c9eb3ce543ae0
erkanoktenn <erkannoktenn@gmail.com>
2024-05-30 22:31:40 -0400

    edited .js get to result to be dynamically created and appended on the website.

--- assets/JavaScript/marvel.js
+++ assets/JavaScript/marvel.js
@@ -1,9 +1,9 @@
 let marvelKey = "1d8194dbe39306bb0084f04db35515ea";
-
+const comicBooksResult = $("#search-results-left");
+const comicCharacterResult = $("#comicbooks")
 function getMarvelApi(characterNameInput) {
   const mainUrl = `https://gateway.marvel.com`;
   const requestUrl = `${mainUrl}/v1/public/characters?nameStartsWith=${characterNameInput}&apikey=${marvelKey}`;
-
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -11,72 +11,59 @@ function getMarvelApi(characterNameInput) {
     .then(function (data) {
       const results = data.data.results[0];
       // console.log(data)
-
       const characterId = results.id;
       // console.log(characterId);
-
       searchByCharacterId(characterId);
-
-      const characterDescription = results.description;
-      console.log(characterDescription);
-
-      const characterComics = results.comics.available;
-      console.log(characterComics)
-      // console.log(results);
-
       const characterName = results.name;
       console.log(characterName);
-
+      $("#search-results-left").empty();
+      const characterInfo = $("<div>");
+      characterInfo.addClass("info card").css({ height: "300px" });
+      const cardHeader = $("<h1>").text(characterName);
+      const characterDescription = results.description;
+      console.log(characterDescription);
+      const description = $("<p>").text(characterDescription);
+      const characterComics = results.comics.available;
+      console.log(characterComics);
+      const comics = $("<p>").text("Available Comics: " + characterComics);
+      // console.log(results);
       const characterSeries = results.series.available;
       console.log(characterSeries);
-
+      const series = $("<p>").text("Available Series: " + characterSeries);
       const characterStories = results.stories.available;
       console.log(characterStories);
-      const exampleComicBookName = results.comics.items[0];
-      // console.log(exampleComicBookName);
-
-      const firstComicBookName = results.comics.items[0].name;
-      console.log(firstComicBookName);
-      const secondComicBookName = results.comics.items[1].name;
-      console.log(secondComicBookName);
-      const thirdComicBookName = results.comics.items[2].name;
-      console.log(thirdComicBookName);
-
-
+      const stories = $("<p>").text("Available Stories: " + characterStories);
+      characterInfo.append(cardHeader, description, comics, series, stories);
+      comicBooksResult.append(characterInfo);
     });
 }
-
 function searchByCharacterId(characterId) {
-    const mainUrl = `https://gateway.marvel.com`;
-    const requestUrl = `${mainUrl}/v1/public/characters/${characterId}/comics?&apikey=${marvelKey}`;
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        const resultsOne = data.data.results[0];
-        console.log(resultsOne);
-        const comicUrl = resultsOne.urls[0].url;
-        console.log(comicUrl);
-        const thumbnailOne = `${resultsOne.thumbnail.path}.${resultsOne.thumbnail.extension}`;
-        console.log(thumbnailOne);
-        const resultsTwo = data.data.results[1];
-        console.log(resultsTwo);
-        const comicUrlTwo = resultsTwo.urls[0].url;
-        console.log(comicUrlTwo);
-        const thumbnailTwo = `${resultsTwo.thumbnail.path}.${resultsTwo.thumbnail.extension}`;
-        console.log(thumbnailTwo);
-        const resultsThree = data.data.results[2];
-        console.log(resultsThree);
-        const comicUrlThree = resultsThree.urls[0].url;
-        console.log(comicUrlThree);
-        const thumbnailThree = `${resultsThree.thumbnail.path}.${resultsThree.thumbnail.extension}`;
-        console.log(thumbnailThree);
-      });
-  }
-
-
-
-
+  const mainUrl = `https://gateway.marvel.com`;
+  const requestUrl = `${mainUrl}/v1/public/characters/${characterId}/comics?&apikey=${marvelKey}`;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // Clear any existing content
+      $("#comicbooks").empty();
+      const comicsSection = $("<div>").addClass("comicsSection");
+      // Loop through the first three results
+      for (let i = 0; i <= 3; i++) {
+        const result = data.data.results[i];
+        const thumbnailUrl = `${result.thumbnail.path}.${result.thumbnail.extension}`;
+        const comicUrl = result.urls[0].url;
+        // Create image element
+        const image = $("<img>").attr("src", thumbnailUrl);
+        // Create link element
+        const link = $("<a>")
+          .attr("href", comicUrl)
+          .attr("target", "_blank")
+          .append(image);
+        // Append link to comics section
+        $(".comicsSection").append(link);
+        comicCharacterResult.append(comicsSection);
+      }
+    });
+}
